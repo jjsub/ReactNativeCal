6b14400ab72c9a00fc5883cae225ec2892268c24
jjsub <jsamesub@gmail.com>
2016-10-18 17:22:22 -0500

    Hello app

---
+++ src/ReactCalculator.js
@@ -0,0 +1,19 @@
+// src/ReactCalculator.js
+
+import React, { Component } from 'react';
+import {
+    Text,
+    AppRegistry
+} from 'react-native';
+
+class ReactCalculator extends Component {
+
+    render() {
+        return (
+            <Text>Hello, React!</Text>
+        )
+    }
+
+}
+
+AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
