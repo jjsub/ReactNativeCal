e38b732278b81af2b2c2f559a7bed4a62d58bbdd
jjsub <jsamesub@gmail.com>
2016-10-18 17:54:40 -0500

    Add style file

--- src/ReactCalculator.js
+++ src/ReactCalculator.js
@@ -2,18 +2,23 @@
 
 import React, { Component } from 'react';
 import {
+    View,
     Text,
+    StyleSheet,
     AppRegistry
 } from 'react-native';
 
-class ReactCalculator extends Component {
+import Style from './style';
 
+class ReactCalculator extends Component {
     render() {
         return (
-            <Text>Hello, React!</Text>
-        )
+          <View style={Style.rootContainer}>
+              <View style={Style.displayContainer}></View>
+              <View style={Style.inputContainer}></View>
+          </View>
+      )
     }
-
 }
 
 AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
